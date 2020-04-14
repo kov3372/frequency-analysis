7e2f9d2afaca3dc36ebec2131b27af89beabfe0a
kov3372 <oleg.koval921@gmail.com>
2020-04-14 11:28:26 +0300

    delete space

--- frequency-analysis/Program.cs
+++ frequency-analysis/Program.cs
@@ -13,46 +13,31 @@ namespace frequency_analysis
             string pathForNormalText = @"c:\text\text33.txt";
             string pathForEncodedText = @"c:\text\encodingText3.txt";
 
-
             string NormalTextFromMetod = ReadТNormalTextFromFile(pathForNormalText);
                   
-
-
             string[] ukA = { "А", "Б", "В", "Г", "Ґ", "Д", "Є", "Ж", "З", "И", "І", "Ї", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ь", "Ю", "Я" };
             string[] rusA = { "А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
-
-
 
             byte[] bytesUTF8 = Encoding.UTF8.GetBytes(NormalTextFromMetod);
             byte[] bytesWin1251 = Encoding.GetEncoding(1251).GetBytes(NormalTextFromMetod);
             byte[] bytesCp866 = Encoding.GetEncoding(866).GetBytes(NormalTextFromMetod);
             byte[] byteKoiu8 = Encoding.GetEncoding("KOI8-R").GetBytes(NormalTextFromMetod);
 
-
-
-
-
             string newStrWin1251 = Encoding.GetEncoding(1251).GetString(bytesWin1251);
             string newStrCp866 = Encoding.GetEncoding(866).GetString(bytesWin1251);
             string newStrUTF8 = Encoding.UTF8.GetString(bytesWin1251);
             string newStrKoui8 = Encoding.GetEncoding("KOI8-U").GetString(byteKoiu8);
 
-
-
-
             double[] appearanceOfTheSymbolInTheories = new double[ukA.Length];
 
             //  CountOfCharactersInNormalTextExample(ukA, NormalTextFromMetod, appearanceOfTheSymbolInTheories);
             CountOfCharactersInNormalText(ukA, NormalTextFromMetod, appearanceOfTheSymbolInTheories);
-
-          
-
+     
             for (int i = 0; i < appearanceOfTheSymbolInTheories.Length; i++)
             {
                 Console.WriteLine(appearanceOfTheSymbolInTheories[i]);
             }
         }
-
 
         public static string ReadТNormalTextFromFile(string pathForNormalText)
         {
@@ -64,9 +49,7 @@ namespace frequency_analysis
             }
             return text;
         }
-
-     
-
+  
         public static void CountOfCharactersInNormalTextExample(string[] ukA, string NormalTextFromMetod, double[] appearanceOfTheSymbolInTheories1)
         {
             int i = 0;
@@ -105,14 +88,9 @@ namespace frequency_analysis
                 double Freq = ((double)count1  / (double)count2);
                 appearanceOfTheSymbolInTheories1[p] = Freq;
                 p++;       
-               // Console.WriteLine("{0}  {1}", Freq, c);
+                Console.WriteLine("{0}  {1}", Freq, c);
             }
-        }
-
-
-
-
-       
+        }   
         }
     }
 
