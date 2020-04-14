61b1f5541215b8627a4c766ffe79bde646fd2387
kov3372 <oleg.koval921@gmail.com>
2020-04-14 11:25:42 +0300

    update project

--- frequency-analysis/Program.cs
+++ frequency-analysis/Program.cs
@@ -10,30 +10,53 @@ namespace frequency_analysis
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            string pathForNormalText = @"c:\text\textForStatistics.txt";
-            string pathForEncodedText = @"c:\text\textForEncoding.txt";
+            string pathForNormalText = @"c:\text\text33.txt";
+            string pathForEncodedText = @"c:\text\encodingText3.txt";
 
 
             string NormalTextFromMetod = ReadТNormalTextFromFile(pathForNormalText);
-            string EncodingText = CodingText(NormalTextFromMetod);
-         //   Console.WriteLine(EncodingText);
+                  
 
 
             string[] ukA = { "А", "Б", "В", "Г", "Ґ", "Д", "Є", "Ж", "З", "И", "І", "Ї", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ь", "Ю", "Я" };
             string[] rusA = { "А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
-            double[] appearanceOfTheSymbolInTheories1 = new double[rusA.Length];
-            double[] appearanceOfTheSymbolInTheories2 = new double[rusA.Length];
-            double[] appearanceOfTheSymbolInTheories3 = new double[rusA.Length];
 
 
-          CountOfCharactersInNormalText(rusA, NormalTextFromMetod,  appearanceOfTheSymbolInTheories1);
+
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(NormalTextFromMetod);
+            byte[] bytesWin1251 = Encoding.GetEncoding(1251).GetBytes(NormalTextFromMetod);
+            byte[] bytesCp866 = Encoding.GetEncoding(866).GetBytes(NormalTextFromMetod);
+            byte[] byteKoiu8 = Encoding.GetEncoding("KOI8-R").GetBytes(NormalTextFromMetod);
 
 
+
+
+
+            string newStrWin1251 = Encoding.GetEncoding(1251).GetString(bytesWin1251);
+            string newStrCp866 = Encoding.GetEncoding(866).GetString(bytesWin1251);
+            string newStrUTF8 = Encoding.UTF8.GetString(bytesWin1251);
+            string newStrKoui8 = Encoding.GetEncoding("KOI8-U").GetString(byteKoiu8);
+
+
+
+
+            double[] appearanceOfTheSymbolInTheories = new double[ukA.Length];
+
+            //  CountOfCharactersInNormalTextExample(ukA, NormalTextFromMetod, appearanceOfTheSymbolInTheories);
+            CountOfCharactersInNormalText(ukA, NormalTextFromMetod, appearanceOfTheSymbolInTheories);
+
+          
+
+            for (int i = 0; i < appearanceOfTheSymbolInTheories.Length; i++)
+            {
+                Console.WriteLine(appearanceOfTheSymbolInTheories[i]);
+            }
         }
+
 
         public static string ReadТNormalTextFromFile(string pathForNormalText)
         {
-            string[] readText = File.ReadAllLines(pathForNormalText);
+            string[] readText = File.ReadAllLines(pathForNormalText,Encoding.GetEncoding(866));
             string text = "";
             for (int i = 0; i < readText.Length; i++)
             {
@@ -42,25 +65,26 @@ namespace frequency_analysis
             return text;
         }
 
+     
 
-        public static void CountOfCharactersInNormalTextExample(string[] rusA, string NormalTextFromMetod, double[] appearanceOfTheSymbolInTheories1)
+        public static void CountOfCharactersInNormalTextExample(string[] ukA, string NormalTextFromMetod, double[] appearanceOfTheSymbolInTheories1)
         {
             int i = 0;
-            foreach (var c in rusA)
+            foreach (var c in ukA)
             {
-                double prc = (double)(100 * NormalTextFromMetod.ToUpper().Count(s => s.ToString() == c)) / (double)(NormalTextFromMetod.ToUpper().Count(s => rusA.Any(p => p == s.ToString())));
+                double prc = (double)( NormalTextFromMetod.ToUpper().Count(s => s.ToString() == c)) / (double)(NormalTextFromMetod.ToUpper().Count(s => ukA.Any(p => p == s.ToString())));
                 appearanceOfTheSymbolInTheories1[i] = prc;
                 i++;
                 Console.WriteLine("{0}  {1}", prc, c);
             }
         }
 
-
-        public static void CountOfCharactersInNormalText(string[] rusA, string NormalTextFromMetod, double[] appearanceOfTheSymbolInTheories1)
+        public static void CountOfCharactersInNormalText(string[] ukA, string NormalTextFromMetod, double[] appearanceOfTheSymbolInTheories1)
         {
+            int p = 0;
             string NormalTextFromMetodUp = NormalTextFromMetod.ToUpper();
             char[] textInChar = NormalTextFromMetodUp.ToCharArray();
-            double count2 = 0;
+            int count2 = 0;
             for (int i = 0; i < textInChar.Length; i++)
             {
                 if (char.IsLetter(textInChar[i]))
@@ -68,7 +92,7 @@ namespace frequency_analysis
                     count2++;
                 }
             }
-            foreach (var c in rusA)
+            foreach (var c in ukA)
             {
                 int count1 = 0;
                 foreach (char s in textInChar)
@@ -78,32 +102,17 @@ namespace frequency_analysis
                         count1++;
                     }
                 }
-                double Freq = (double)count1 * 100 / count2;
-                Console.WriteLine("{0}  {1}", Freq, c);
+                double Freq = ((double)count1  / (double)count2);
+                appearanceOfTheSymbolInTheories1[p] = Freq;
+                p++;       
+               // Console.WriteLine("{0}  {1}", Freq, c);
             }
         }
 
 
 
 
-
-        public static string CodingText(string NormalTextFromMetod)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            byte[] bytes = Encoding.UTF8.GetBytes(NormalTextFromMetod);
-            byte[] newBytes = Encoding.Convert(Encoding.UTF8, Encoding.GetEncoding(866), bytes); 
-            string newStr = Encoding.GetEncoding(1251).GetString(newBytes);
-            return newStr;
+       
         }
-
-
-
-
-
-
-      
-
-
-
     }
-}
+
